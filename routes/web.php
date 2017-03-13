670e08518d680ac45e980ca6e4068a2bc48bda0d
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-03-12 22:59:50 -0300

    Alteraçao teste - admin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('/contrato/novo', function(){
+Route::get('/contrato', function(){
 	return redirect('/contrato/novo');
 });
 
